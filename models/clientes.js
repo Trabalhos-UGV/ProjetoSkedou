626ef697ddbb228ebb9fc4606c7e645e8a2f30a9
Annie <ens-rafaelsobrinho@ugv.edu.br>
2025-09-13 23:38:27 -0300

    Style: Adicionado comentarios nos models

--- models/clientes.js
+++ models/clientes.js
@@ -2,14 +2,17 @@ const { DataTypes } = require('sequelize')
 const sequelize = require('../src/db/cnx');
 
 const clientes = sequelize.define("clientes", {
+    //codigo do cliente
     cli_cod: {
         type: DataTypes.BIGINT,
         primaryKey: true,
         autoIncrement: true
     },
+    //usuário do cliente, foreign key da tabela usuarios
     cli_usr:{
         type: DataTypes.BIGINT
     },
+    //nome do cliente
     cli_nom:{
         type: DataTypes.STRING(50),
         allowNull: false
@@ -17,6 +20,7 @@ const clientes = sequelize.define("clientes", {
 },
 {
     tableName: 'clientes',
+    //sem timestamp
     timestamps: false
 })
 
--- models/viewHistorico.js
+++ models/viewHistorico.js
@@ -2,18 +2,22 @@ const { DataTypes } = require('sequelize')
 const sequelize = require('../src/db/cnx');
 
 const historico = sequelize.define('preco_lista', {
+    //nome do cliente em clientes.cli_nom (esta lsitado como chave primaria pois senão o sequelize adiciona 'id' na busca)
     nome_cliente: {
         type: DataTypes.STRING(50),
         allowNull: false,
         primaryKey: true
     },
+    //data do ultimo agendamento em solicitacoes.sol_dat
     ultimo_agendamento: {
         type: DataTypes.DATEONLY,
         allowNull: false
     },
+    //quantidade de servicos feitos em lista_servicos.lis_sev_cod (está contando a quantidade servicos agrupando pelo usr_cod)
     quan_servicos: {
         type: DataTypes.BIGINT
     },
+    //Valor total dos servicos
     valor_solicitacao: {
         type: DataTypes.NUMBER
     }
