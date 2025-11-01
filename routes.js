efc83b95e0f5f7d99746cca5953ab715bba47d81
Annie <ens-rafaelsobrinho@ugv.edu.br>
2025-11-01 16:17:46 -0300

    UPDARTE: funcionalidades menores

--- routes.js
+++ routes.js
@@ -1,23 +1,24 @@
 //Requires do sistema
 
-//Express
-  const express = require('express')
+  //Express
+    const express = require('express')
 
-//Router do express
-  const router = express.Router()
+  //Router do express
+    const router = express.Router()
 
-//Path
-  const path = require('path')
+//Requires dos controllers
 
-//Rota padrão, por enquanto nada nela
+  //Usuário
+    const usuariosController = require('./controllers/usuariosController')
+
+//Rota padrão, mostra as rotas de api
   router.get('/', (req, res) => {
-    res.send('Hello, World!')
+    res.sendFile(__dirname + '/src/index.html')
   })
 
-//Pagina de login, a rota é about e o arquivo é landpage pqp KKKKKKKKKKKKKKKKKKKKKKKKKKK
-  router.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'html', 'landPage.html'))
-  })
+//Rota de cadastro do usuário
+  router.post('/api/usuarios/cadastro', usuariosController.cadastrarUsuario)
+  router.post('/api/usuarios/login', usuariosController.loginUsuario)
 
 //Imagem 404 quando uma rota não é encontrada
   router.use((req, res, next) => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 42069;
 const router = require("../routes");
 const sequelize = require('../src/db/cnx')
 const cors = require('cors')
